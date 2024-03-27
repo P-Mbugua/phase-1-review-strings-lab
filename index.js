bb889b0164b073539695dd0a394b0bfbf4b450aa
pmbugua <pmbugua276@gmail.com>
2024-03-27 13:33:10 +0300

    completed the lab successfully

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser  = "peter Mbugua";
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
